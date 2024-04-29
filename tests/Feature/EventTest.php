a79edc46196a4d8884b31d9a5e911539e37cd2d1
theo550 <theo.gaudin35@gmail.com>
2024-04-29 12:24:58 +0200

    Create Event Test

---
+++ tests/Feature/EventTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Models\Event;
+
+test('return all events', function () {
+    $events = Event::factory()->count(3)->create();
+    $response = $this->get('/events');
+
+    expect($response)->assertStatus(200);
+    expect(count($response))->toBe(3);
+});
